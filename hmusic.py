50cda52bc4b391024f492d94f6a2e409729d5031
Daniel Thiong'o <109515278+danthio@users.noreply.github.com>
2025-07-23 18:15:19 +0300

    Update hmusic.py

--- hmusic.py
+++ hmusic.py
@@ -7860,7 +7860,7 @@ im.save("data/bg.png")
 
 
 #darken_image("data/bg.png", "data/bg2.png",(0,0,0), opacity=0.6)
-darken_image("data/bg.png", "data/bg.png",(0,0,0), opacity=0.4)
+darken_image("data/bg.png", "data/bg.png",(0,0,0), opacity=0.5)
 
 
 
