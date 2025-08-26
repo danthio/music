6305269df552ba47ea9eefc16b15c6aca9fe64cb
Daniel Thiong'o <109515278+danthio@users.noreply.github.com>
2025-08-26 12:57:42 +0300

    Update hmusic.py

--- hmusic/hmusic.py
+++ hmusic/hmusic.py
@@ -6777,7 +6777,7 @@ def draw_can(con=0):
         c.create_polygon(ar,fill=col,outline=col)
 
 
-    #draw_round_rec2(can,0,0,w-1,h-1,25,"#333333")
+    draw_round_rec2(can,0,0,w-1,h-1,25,"#000000")
 
 
 
@@ -10503,10 +10503,12 @@ def can_settings_b1(e):
 
             settings_st=1
 
+            no_bg_st=0
+
             draw_settings(1)
 
 
-            no_bg_st=0
+            
 
         except:
             pass
