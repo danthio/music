9b4ab8d2ef65e6f4a265aff57b23403b77c8fa28
Daniel Thiong'o <109515278+danthio@users.noreply.github.com>
2026-02-26 11:41:55 +0300

    Add files via upload

--- hmusic/hmusic.py
+++ hmusic/hmusic.py
@@ -5164,7 +5164,7 @@ def check_volume():
         vol2=can.create_text(w-10,h-20-30+5+10-3+12,text=str(int(current_volume*100))+"%",fill=col1,font=("FreeMono",11),anchor="e")
 
         vol3=can.create_image(w-10-120+current_volume*r,h-20-30+5+10-3,image=circle7,anchor="c")
-        draw_cur_can()
+        draw_can()
 
     root.after(500,check_volume)
 
@@ -8348,7 +8348,7 @@ def draw_can(con=0):
             xx,yy=int((w/2-10-40)),70
 
             col=hex_to_rgb(_theme[0])
-            col=(int(col[0]*0.125),int(col[1]*0.125),int(col[2]*0.125))
+            col=(int(col[0]*0.1),int(col[1]*0.1),int(col[2]*0.1))
             col="#%02x%02x%02x" % col
 
             im1=round_im(col,_theme[0],0.8,xx,yy,r,1)
@@ -9997,7 +9997,7 @@ def load_im():
 
     col=hex_to_rgb(_theme[0])
 
-    draw.polygon(ar,fill=(int(col[0]*0.125),int(col[1]*0.125),int(col[2]*0.125),int(round(0.8*255,0))),outline=(int(col[0]*0.125),int(col[1]*0.125),int(col[2]*0.125),int(round(0.9*255,0))))
+    draw.polygon(ar,fill=(int(col[0]*0.1),int(col[1]*0.1),int(col[2]*0.1),int(round(0.8*255,0))),outline=(int(col[0]*0.1),int(col[1]*0.1),int(col[2]*0.1),int(round(0.8*255,0))))
 
     b_g1=ImageTk.PhotoImage(im)
 
@@ -10036,7 +10036,7 @@ def load_im():
     ar.append((w,h-(90+int(can2["height"]))+r))
     ar.append((0,h-(90+int(can2["height"]))+r))
 
-    draw.polygon(ar,fill=(int(col[0]*0.125),int(col[1]*0.125),int(col[2]*0.125),int(round(0.8*255,0))),outline=(int(col[0]*0.125),int(col[1]*0.125),int(col[2]*0.125),int(round(0.9*255,0))))
+    draw.polygon(ar,fill=(int(col[0]*0.1),int(col[1]*0.1),int(col[2]*0.1),int(round(0.8*255,0))),outline=(int(col[0]*0.1),int(col[1]*0.1),int(col[2]*0.1),int(round(0.8*255,0))))
 
     b_g2=ImageTk.PhotoImage(im)
 
